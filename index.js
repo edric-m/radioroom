27fbf4c6502dde3dbf220a9e597c7d852909436b
edric <edjomendoza@gmail.com>
2019-07-26 17:45:36 +1000

    testing how player will work

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+//var db = require('./create_mongo_db');
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/list', function(req,res){
+    res.sendFile(__dirname + '/list.html');
+});
+
+app.get('/option', function(req,res){
+    res.sendFile(__dirname + '/option.html');
 });
 
 io.on('connection', function(socket){
