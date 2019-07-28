4dfda6ead9933128d14df9a41853a50ad43669b3
edric <edjomendoza@gmail.com>
2019-07-28 10:44:51 +1000

    trying to clean project

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var io = require('socket.io')(http);
 //var db = require('./create_mongo_db');
 
 app.get('/', function(req,res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/client/index.html');
 });
 
 app.get('/list', function(req,res){
-    res.sendFile(__dirname + '/list.html');
+    res.sendFile(__dirname + '/client/list.html');
 });
 
 app.get('/option', function(req,res){
-    res.sendFile(__dirname + '/option.html');
+    res.sendFile(__dirname + '/client/option.html');
 });
 
 io.on('connection', function(socket){
